7db82efb871417e5f9806462da1aabbe301c2a94
guille <jgyanez1@gmail.com>
2021-08-05 00:28:56 -0400

    inicio de Proyecto

---
+++ Backend/src/main/java/com/test/backend/controller/HomeController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test.backend.controller;
+
+/**
+ *
+ * @author Jose Guillermo Yanez
+ */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+/**
+ *
+ * @author Jose Guillermo Yanez
+ */
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class HomeController {
+    
+    @GetMapping("/home")
+    public String home(){
+          return "Test de Inicio Aplicación Test Tecnico. ";
+    }
+    
+ 
+}
